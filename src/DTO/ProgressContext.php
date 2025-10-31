214688212bdf2a405add6385cc9bcaac89763f48
tahyhax <tahyhax456@gmail.com>
2025-10-31 13:39:22 +0200

    feat(scorm-api): add `ProgressContext` DTO for progress tracking and job metadata

---
+++ src/DTO/ProgressContext.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace OnixSystemsPHP\HyperfScorm\DTO;
+
+use OnixSystemsPHP\HyperfCore\DTO\AbstractDTO;
+
+/**
+ * Context for progress tracking
+ * Immutable value object carrying job identification data
+ *
+ * @property string $jobId Job identifier for tracking
+ * @property int $userId User who initiated the upload
+ * @property int $fileSize Size of uploaded file in bytes
+ * @property bool $isRetryable Whether the job can be retried on failure
+ */
+class ProgressContext extends AbstractDTO
+{
+    public string $jobId;
+    public int $userId;
+    public int $fileSize;
+
+    /**
+     * Whether this job can be retried on failure
+     *
+     * - true: Job can retry (attempts 1-2 of 3) - don't notify user on error
+     * - false: Final attempt or called from Job::failed() - notify user on error
+     */
+    public bool $isRetryable = true;
+}
